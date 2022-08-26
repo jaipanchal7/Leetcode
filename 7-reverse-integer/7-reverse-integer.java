94b9b786b8b50bee514f92fcdd7fbd5c6530abdc
Jai <jaipanchal100@gmail.com>
2022-08-26 18:39:11 +0530

    Time: 2 ms (78.43%), Space: 41.3 MB (57.94%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        
+        long sum=0;
+        // long rem=0;
+        
+        while(x!=0){
+            // rem = x%10;
+            sum = (sum * 10) + x%10;
+            x /= 10;
+        }
+        
+        if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
+            return 0;
+        } else {
+            return (int)sum;
+        }
+        
+        
+    }
+}
