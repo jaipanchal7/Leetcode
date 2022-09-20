ae840f2643a04915ba9877965cfac24d5a8b7026
Jai <jaipanchal100@gmail.com>
2022-09-20 15:51:20 +0530

    Time: 15 ms (43.82%), Space: 43.7 MB (36.12%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger str1 = new BigInteger(num1);
+        BigInteger str2 = new BigInteger(num2);
+        
+        BigInteger ans = str1.multiply(str2);
+        return ans.toString();
+    }
+}
