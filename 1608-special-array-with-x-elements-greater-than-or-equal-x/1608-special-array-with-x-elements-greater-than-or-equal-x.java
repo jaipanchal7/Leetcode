394109317da612ba49acffa40402cc599543cc8e
Jai <jaipanchal100@gmail.com>
2022-09-23 14:56:02 +0530

    Time: 2 ms (60.94%), Space: 42.8 MB (5.66%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -3,7 +3,7 @@ class Solution {
         for (int i = 0; i<nums.length + 1; i++){
             int count = 0;
             for (int j = 0; j<nums.length; j++){
-                if (i<=nums[j]){
+                if (nums[j]>=i){
                     count ++;
                 }
             }
