7bde4e8dbe233f0b093b003bfe224a307e06b8f2
Jai <jaipanchal100@gmail.com>
2022-09-27 14:39:24 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (96.94%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        String[] str = s.split(" ");
+        int ans = str.length-1;
+        return str[ans].length();
+        
+    }
+}
