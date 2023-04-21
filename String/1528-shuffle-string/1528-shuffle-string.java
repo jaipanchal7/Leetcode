daf45146df5e87890fe3d9680b71c3b67c5409e5
Jai <jaipanchal100@gmail.com>
2023-04-21 15:01:40 +0530

    sub folders

--- String/1528-shuffle-string/1528-shuffle-string.java
+++ String/1528-shuffle-string/1528-shuffle-string.java
@@ -9,7 +9,7 @@ class Solution {
         
         char[] charArray = new char[indices.length];
 		
-        for(int i=0;i<indices.length;i++){
+        for(int i=0; i<indices.length; i++){
             charArray[indices[i]] = s.charAt(i);
         }
         return new String(charArray);
